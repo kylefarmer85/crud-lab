d03a3c6f33eb1e352b0a29419a2f726d3eff7b5b
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-02 00:07:38 -0600

    delete restaurant working

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
+
+  renderRestaurants = () => {
+    return this.props.restaurants.map(restaurant => {
+      return <Restaurant restaurant={restaurant} deleteRestaurant={this.props.deleteRestaurant} />
+    })
+  }
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        Restaurants:
+      { this.renderRestaurants() }
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,30 @@
-import cuid from 'cuid';
+import { v4 as uuidv4 } from 'uuid';
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state= {
+  restaurants: [],
+  reviews: []
+}, action) {
 
+  let updatedRestaurants;
+
+  switch (action.type) {
+  
+    case "ADD_RESTAURANT":
+
+      const restaurant = {text: action.text, id: uuidv4()}
+      return {
+        ...state, 
+        restaurants: [...state.restaurants, restaurant]
+      }
+      
+    case "DELETE_RESTAURANT":
+        updatedRestaurants = state.restaurants.filter(r => r.id !== action.id)   
+      return {
+        ...state,
+        restaurants: updatedRestaurants
+      }
+
+    default:
+      return state  
+  }
 }
